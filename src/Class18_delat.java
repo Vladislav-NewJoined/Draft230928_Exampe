10e231f96865a01014599b9cefe28eb9ed04fdc8
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-11-03 08:35:50 +0300

    Initial commit

--- src/Class18_delat.java
+++ src/Class18_delat.java
@@ -4,6 +4,7 @@
 // Конструктор  _ +java конструктор примеры _ с Class27
 // источник: https://www.youtube.com/results?search_query=%2Bjava+%D0%BA%D0%BE%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%82%D0%BE%D1%80+%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B
 // источник: https://javarush.com/groups/posts/1391-konstruktorih-klassov-java-jdk-15
+// источник: https://www.youtube.com/playlist?list=PLqj7-hRTFl_oDMBjI_EstsFcDAwt-Arhs - прокаченная Java Заур Трегулов
 // delat источник: https://www.youtube.com/watch?v=Muytoo-x-KM
 // источник: https://www.youtube.com/watch?v=lSPesgbQQRc
 // источник: https://www.youtube.com/watch?v=lSPesgbQQRc
